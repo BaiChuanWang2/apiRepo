b52414b6e5c46175ab86cb9585f9296ebc70c151
BaiChuan Wang <baichuan.wang2@gmail.com>
2026-02-19 21:12:28 +0900

    update main fun

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def main():
+    return {"message": "API is running"}
+
 @app.get("/test")
 def test():
     return {"message": "Hello from FastAPI!"}
